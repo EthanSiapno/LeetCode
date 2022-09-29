a48b700668c3a1ee56ff09ee69a4815263c19fc5
EthanSiapno <EKSiapno@gmail.com>
2022-09-29 12:09:43 -0400

    Time: 64 ms (13.48%), Space: 13.9 MB (7.31%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -1,3 +1,19 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        """
+        Trivial solution below:
+        """
+        # return s.lower()
+        
+        """
+        My attempt at a solution without using built-in features.
+        """
+        if len(s) == 0:
+            return s
+        output = ''
+        for i in range(0, len(s)):
+            if ord(s[i]) >= 65 and ord(s[i]) <= 90:
+                output += chr(ord(s[i]) + 32)
+            else:
+                output += s[i]
+        return output
