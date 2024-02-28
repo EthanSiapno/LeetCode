77a08cde40faad847b9ac263a69ccfce4aaf741a
EthanSiapno <EKSiapno@gmail.com>
2024-02-28 18:47:33 -0500

    Time: 99 ms (21.30%), Space: 21.8 MB (45.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,32 +1,50 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countDict = {}
-        countList = []
-        outputList = []
-        highestKey = 0
-        highestVal = 0
+        count = {}
+        frequency = [[] for i in range(len(nums)+1)]
         
         for num in nums:
-            if countDict.get(num,0) == 0:
-                countDict[num] = 1
-            else:
-                countDict[num] += 1
+            count[num] = 1 + count.get(num,0)
+        for key, val, in count.items() :
+            frequency[val].append(key)
         
-        for key in countDict:
-            countList.append(tuple([key, countDict[key]]))
+        output = []
         
-        # print(countList)
+        for i in range(len(frequency)-1, 0, -1):
+	        for n in frequency[i]:
+		        output.append(n)
+		        if k == len(output):
+			        return output
+            
         
-        while k > 0:
-            for pair in countList:
-                if pair[1] > highestVal:
-                    if pair[0] not in outputList:
-                        highestVal = pair[1]
-                        highestKey = pair[0]
+        
+#         countDict = {}
+#         countList = []
+#         outputList = []
+#         highestKey = 0
+#         highestVal = 0
+        
+#         for num in nums:
+#             if countDict.get(num,0) == 0:
+#                 countDict[num] = 1
+#             else:
+#                 countDict[num] += 1
+        
+#         for key in countDict:
+#             countList.append(tuple([key, countDict[key]]))
+        
+#         # print(countList)
+        
+#         while k > 0:
+#             for pair in countList:
+#                 if pair[1] > highestVal:
+#                     if pair[0] not in outputList:
+#                         highestVal = pair[1]
+#                         highestKey = pair[0]
                 
-            outputList.append(highestKey)
-            highestKey = 0
-            highestVal = 0
-            k -= 1
+#             outputList.append(highestKey)
+#             highestKey = 0
+#             highestVal = 0
+#             k -= 1
         
-        return outputList
+#         return outputList
