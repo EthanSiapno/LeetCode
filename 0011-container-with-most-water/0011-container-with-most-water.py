7068bec065b764f8d0286befeda18b546af373ea
EthanSiapno <EKSiapno@gmail.com>
2024-03-06 18:33:04 -0500

    Time: 496 ms (89.71%), Space: 29.3 MB (91.50%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,9 +3,6 @@ class Solution:
         l, r = 0, len(height) - 1
         maximum = 0
         temp = 0
-        temp = (r - l) * (min(height[l], height[r]))
-        # print(temp)
-
         
         while l < r:
             temp = (r - l) * (min(height[l], height[r]))
