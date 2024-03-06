5ff9ff0607012f0902a678498b0442516e7412c2
EthanSiapno <EKSiapno@gmail.com>
2024-03-06 02:18:58 -0500

    Time: 95 ms (42.07%), Space: 16.6 MB (40.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,81 +1,100 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        tempNine = []
-        validOrd = [i for i in range(ord('1'),ord('9')+1)]
-        validOrd.append(ord('.'))
-        numOrd = [i for i in range(ord('1'),ord('9')+1)]
-        tempTop = []
-        tempMid = []
-        tempBot = []
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        squares = defaultdict(set)
         
-        #rows:
-        #ord("1") = 49
-        #ord("9") = 57
-        #ord(".") = 46
-        for row in range(9):
-            for num in board[row]:
-                # print(ord(num))
-                if len(num) > 1 or ord(num) not in validOrd:
-                    print("ONE")
+        for row in range(0,9):
+            for col in range(0,9):
+                if board[row][col] == ".":
+                    continue
+                if (board[row][col] in rows[row] or
+                    board[row][col] in columns[col] or
+                    board[row][col] in squares[(row // 3), (col // 3)]
+                ):
                     return False
-                if ord(num) in numOrd:
-                    # print(num)
-                    if int(num) in tempNine:
-                        print("TWO")
-                        return False
-                    else:
-                        tempNine.append(int(num))
-            tempNine = []
+                rows[row].add(board[row][col])
+                columns[col].add(board[row][col])
+                squares[(row // 3, col // 3)].add(board[row][col])
+        
+        return True
+                    
+#         tempNine = []
+#         validOrd = [i for i in range(ord('1'),ord('9')+1)]
+#         validOrd.append(ord('.'))
+#         numOrd = [i for i in range(ord('1'),ord('9')+1)]
+#         tempTop = []
+#         tempMid = []
+#         tempBot = []
+        
+#         #rows:
+#         #ord("1") = 49
+#         #ord("9") = 57
+#         #ord(".") = 46
+#         for row in range(9):
+#             for num in board[row]:
+#                 # print(ord(num))
+#                 if len(num) > 1 or ord(num) not in validOrd:
+#                     print("ONE")
+#                     return False
+#                 if ord(num) in numOrd:
+#                     # print(num)
+#                     if int(num) in tempNine:
+#                         print("TWO")
+#                         return False
+#                     else:
+#                         tempNine.append(int(num))
+#             tempNine = []
             
-            for col in range(9):
-                if len(board[col][row]) > 1 or ord(board[col][row]) not in validOrd:
-                    print("Three")
-                    return False
-                if ord(board[col][row]) in numOrd:
-                    if int(board[col][row]) in tempNine:
-                        print("Four")
-                        return False
-                    else:
-                        # print(int(board[col][row]))
-                        tempNine.append(int(board[col][row]))
-                        # print(tempNine)
+#             for col in range(9):
+#                 if len(board[col][row]) > 1 or ord(board[col][row]) not in validOrd:
+#                     print("Three")
+#                     return False
+#                 if ord(board[col][row]) in numOrd:
+#                     if int(board[col][row]) in tempNine:
+#                         print("Four")
+#                         return False
+#                     else:
+#                         # print(int(board[col][row]))
+#                         tempNine.append(int(board[col][row]))
+#                         # print(tempNine)
                         
-                    if col < 3:
-                        if int(board[col][row]) in tempTop:
-                            print("Five")
-                            return False
-                        else:
-                            tempTop.append(int(board[col][row]))
-                    elif col >=3 and col < 6:
-                        if int(board[col][row]) in tempMid:
-                            # print(tempTop)
-                            # print(col)
-                            # print(row)
-                            # print(int(board[col][row]))
-                            print("Six")
-                            return False
-                        else:
-                            tempMid.append(int(board[col][row]))
-                    else:
-                        if int(board[col][row]) in tempBot:
-                            print("Seven")
-                            return False
-                        else:
-                            tempBot.append(int(board[col][row]))
-            tempNine = []
+#                     if col < 3:
+#                         if int(board[col][row]) in tempTop:
+#                             print("Five")
+#                             return False
+#                         else:
+#                             tempTop.append(int(board[col][row]))
+#                     elif col >=3 and col < 6:
+#                         if int(board[col][row]) in tempMid:
+#                             # print(tempTop)
+#                             # print(col)
+#                             # print(row)
+#                             # print(int(board[col][row]))
+#                             print("Six")
+#                             return False
+#                         else:
+#                             tempMid.append(int(board[col][row]))
+#                     else:
+#                         if int(board[col][row]) in tempBot:
+#                             print("Seven")
+#                             return False
+#                         else:
+#                             tempBot.append(int(board[col][row]))
+#             tempNine = []
             
-            if row % 3 == 2:
-                # print(tempNine)
-                print(tempTop)
-                # print(tempMid)
-                # print(tempBot)
-                tempTop = []
-                tempMid = []
-                tempBot = []
+#             if row % 3 == 2:
+#                 # print(tempNine)
+#                 print(tempTop)
+#                 # print(tempMid)
+#                 # print(tempBot)
+#                 tempTop = []
+#                 tempMid = []
+#                 tempBot = []
                 
         
         
             
-        return True
+#         return True
 
             
